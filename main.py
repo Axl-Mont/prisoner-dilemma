c72e4af827a5ce8aa499abe0c43f899eb1267650
axl-mont <montenegroaxel@outlook.com>
2024-03-25 19:48:29 -0300

    se termino la base del juego y mejoro la forma de mostras en consola el estado del juego

--- main.py
+++ main.py
@@ -1,75 +1,97 @@
 import pkg
+import time
+import os
 
+def clear_console():
+    """Función para limpiar la consola"""
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-"""def game_status():
-        # Mostrar puntajes de la ronda actual
-        print("Ronda:", ronda)
-        print("Puntaje acumulado del usuario:", puntaje_usuario)
-        print("Puntaje acumulado de la estrategia:", puntaje_estrategia)
-        print()"""
+def print_instrucciones():
+    print("Bienvenido al Dilema del Prisionero\r")
+    time.sleep(1)
+    print("Este es un juego simple donde tú y una estrategia competirán en rondas.")
+    time.sleep(1.5)
+    print("Cada ronda, tienes que decidir si cooperar (O) o no cooperar (X).")
+    time.sleep(1.5)
+    print("Tu puntaje se determina según las decisiones tuyas y de la estrategia.")
+    time.sleep(1.5)
+    print("Si ambos cooperan    0 - 0 suman 3 puntos cada uno.")
+    time.sleep(1.5)
+    print("Si uno no coopera    X - 0 suman 5 puntos quien no coopera.")
+    time.sleep(1.5)
+    print("Si ambos no cooperan 0 - 0 suman 1 puntos cada uno.")
+    time.sleep(1.5)
+    print("El juego termina después de un número aleatorio de rondas entre 10 y 25.")
+    time.sleep(1.5)
+    print("¡Buena suerte y diviértete!")
 
+def print_game_status(user_name, user_moves, strategy_moves):
+    max_length = max(len(user_moves), len(strategy_moves))
 
-def jugada(name, user_history):
+    print("---------------------------------------------")
+    print("                 Movimientos                 ")
+    print("---------------------------------------------")
+
+    user_moves_aligned = user_moves.ljust(max_length)
+    strategy_moves_aligned = strategy_moves.ljust(max_length)
+
+    len_t4t = len("Tit For Tat")
+    len_user = len(user_name)
+    user_spaces = " " * (len_t4t - len_user)
+
+    print(f"{user_spaces}{user_name} {user_moves_aligned}")
+    print(f"Tit For Tat {strategy_moves_aligned}")
+
+def get_user_input(name, user_history):
     print(f'\n {name} ingresa O para cooperar o X para no cooperar')
+    user_choice = input('==> ').upper()
+    strategy_choice = pkg.tit_for_tat.t4t_strategy(user_history)
+    return user_choice, strategy_choice
 
-    
-    user_choise = input('==> ').upper()
-    estatega_choise = pkg.tit_for_tat.t4t_strategy(user_history)
-
-    return user_choise, estatega_choise
-
-
-def puntaje(jugada_usuario, jugada_estratega, puntos_usuario, puntos_estratega):
-    tabla_de_puntos = {
+def calculate_score(user_choice, strategy_choice, user_score, strategy_score):
+    score_table = {
         ('X', 'X'): (1, 1),
         ('X', 'O'): (5, 0),
         ('O', 'X'): (0, 5),
         ('O', 'O'): (3, 3)
     }
-
-    # Obtener el puntaje correspondiente para la jugada actual
-    puntaje_actual = tabla_de_puntos[(jugada_usuario, jugada_estratega)]
-
-    # Sumar los puntos de la jugada actual a los acumulados
-    puntos_usuario += puntaje_actual[0]
-    puntos_estratega += puntaje_actual[1]
-
-    return puntos_usuario, puntos_estratega
-
-
-def usuario():
-    name = input('Ingresa tu nombre de jugador ==> ')
-    return name
-
+    current_score = score_table[(user_choice, strategy_choice)]
+    user_score += current_score[0]
+    strategy_score += current_score[1]
+    return user_score, strategy_score
 
 def main():
+    clear_console()
+    print_instrucciones()
 
-    name_usuario = usuario()
-    puntaje_usuario = 0
-    puntaje_estratega = 0
-    historial_usuario = []
-    historial_estratega = []
-    ronda_maxima = pkg.random.randint(10, 20)
-    turno = 0
+    user_name = input('Ingresa tu nombre para comenzar ==> ').upper()
+    user_score = 0
+    strategy_score = 0
+    user_history = []
+    strategy_history = []
+    round_max = pkg.random.randint(10, 25)
+    round_count = 0
 
-    while turno != ronda_maxima:
+    user_moves = ''
+    strategy_moves = ''
 
-        jugada_usuario,jugada_estratega = jugada(name_usuario, historial_usuario)
+    while round_count < round_max:
+        clear_console()
+        print_game_status(user_name, user_moves, strategy_moves)
 
-        historial_usuario.append(jugada_usuario)
-        historial_estratega.append(jugada_estratega)
+        user_choice, strategy_choice = get_user_input(user_name, user_history)
+        user_history.append(user_choice)
+        strategy_history.append(strategy_choice)
 
-        print(historial_usuario)
-        print(historial_estratega)
+        user_moves = ''.join(user_history)
+        strategy_moves = ''.join(strategy_history)
 
-        puntaje_usuario, puntaje_estratega = puntaje(
-            jugada_usuario, jugada_estratega,
-            puntaje_usuario, puntaje_estratega
-            )
-        print(puntaje_usuario)
-        print(puntaje_estratega)
-        turno += 1
+        user_score, strategy_score = calculate_score(user_choice, strategy_choice, user_score, strategy_score)
 
+        round_count += 1
+
+        if round_count >= round_max:
+            break
 
 if __name__ == "__main__":
     main()
